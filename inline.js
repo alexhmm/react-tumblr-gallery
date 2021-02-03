80fcda28ea0faa7fea6f35597813f2e573f565a7
Alex Home <axel-pi@gmx.de>
2021-02-03 20:44:28 +0100

    Project adjustments: - Add SCSS - Add HTML inline script for tumblr production

---
+++ inline.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const prettier = require('prettier');
+const webResourceInliner = require('web-resource-inliner');
+
+// Read index.html from single page application
+const input = fs.readFileSync('build/index.html', 'utf8');
+
+new webResourceInliner.html(
+  {
+    fileContent: input,
+    relativeTo: 'build'
+  },
+  function(error, result) {
+    if (error) {
+      console.error('Error on formatting result:', error);
+    }
+    // Format file with prettier
+    const formatted = prettier.format(result, {
+      arrowParens: 'avoid',
+      bracketSpacing: true,
+      endOfLine: 'auto',
+      htmlWhitespaceSensitivity: 'css',
+      insertPragma: false,
+      jsxBracketSameLine: false,
+      jsxSingleQuote: true,
+      parser: 'html',
+      printWidth: 80,
+      proseWrap: 'preserve',
+      quoteProps: 'consistent',
+      requirePragma: false,
+      semi: true,
+      singleQuote: true,
+      tabWidth: 2,
+      trailingComma: 'none',
+      useTabs: false,
+      vueIndentScriptAndStyle: false
+    });
+    // Write inlined output file
+    fs.writeFile('build/inline.html', formatted, 'utf8', function(error) {
+      if (error) {
+        console.error('Error on writing file:', error);
+      } else {
+        console.log('Success on formatting file.');
+      }
+    });
+  }
+);
