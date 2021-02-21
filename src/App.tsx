40be560382bac22fec89fc321463da76a962b190
Alex Home <axel-pi@gmx.de>
2021-02-21 19:53:39 +0100

    Add environment variables, test tumblr posts get

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 
 function App() {
+  let url =
+    process.env.REACT_APP_API_URL +
+    '/posts?api_key=' +
+    process.env.REACT_APP_API_KEY;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(data));
+
   return (
     <div className='App'>
       <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
+        <p>{process.env.REACT_APP_API_URL}</p>
       </header>
     </div>
   );
