9cd50e751720446c67c5cdd653b5a3f472887f4f
Alex Home <axel-pi@gmx.de>
2021-03-11 20:39:42 +0100

    Add basic route fade transition

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,19 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import PostDetail from './posts/pages/post-detail/post-detail.page';
-import Posts from './posts/pages/posts/Posts.page';
+import AppRouter from './shared/components/AppRouter/AppRouter.component';
 import Menu from './shared/components/Menu/Menu.component';
 import Theme from './shared/components/Theme/Theme.component';
 import Title from './shared/components/Title/Title.component';
 
-function App() {
+const App = () => {
   return (
     <Router>
       <Menu />
       <Title />
       <Theme />
-      <Switch>
-        <Route path='/page/:pageNumber'>
-          <Posts />
-        </Route>
-        <Route path='/post/:postId/:caption'>
-          <PostDetail />
-        </Route>
-        <Route path='/post/:postId'>
-          <PostDetail />
-        </Route>
-        <Route path='/tagged/:tag/page/:pageNumber'>
-          <Posts />
-        </Route>
-        <Route path='/tagged/:tagged'>
-          <Posts />
-        </Route>
-        <Route path='/'>
-          <Posts />
-        </Route>
-      </Switch>
+      <AppRouter />
     </Router>
   );
-}
+};
 
 export default App;
---
+++ src/shared/components/AppRouter/AppRouter.component.tsx
@@ -0,0 +1,51 @@
+import { FunctionComponent } from 'react';
+import { Switch, Route, useLocation } from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+
+import PostDetail from '../../../posts/pages/post-detail/post-detail.page';
+import Posts from '../../../posts/pages/posts/Posts.page';
+
+import './AppRouter.scss';
+
+/**
+ * Application routes
+ */
+const routes = [
+  { path: '/', name: 'Home', Component: Posts },
+  { path: '/post/:postId', name: 'About', Component: PostDetail },
+  { path: '/post/:postId/:caption', name: 'Contact', Component: PostDetail },
+  { path: '/tagged/:tagged', name: 'Contact', Component: Posts }
+];
+
+const AppRouter = () => {
+  const location = useLocation();
+
+  return (
+    <TransitionGroup>
+      <CSSTransition
+        timeout={500}
+        classNames='fade'
+        key={location.key}
+        unmountOnExit={true}
+      >
+        <section className='route'>
+          <Switch location={location}>
+            {routes.map(
+              ({
+                path,
+                Component
+              }: {
+                path: string;
+                Component: FunctionComponent;
+              }) => (
+                <Route key={path} exact path={path} children={<Component />} />
+              )
+            )}
+          </Switch>
+        </section>
+      </CSSTransition>
+    </TransitionGroup>
+  );
+};
+
+export default AppRouter;
