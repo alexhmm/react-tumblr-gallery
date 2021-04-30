9877765e467d95ffb30d739b91ba8945adee1fcb
Alex Home <axel-pi@gmx.de>
2021-04-30 13:26:21 +0200

    Post: Modify overlay (hover devices)

--- src/posts/components/post/post.component.tsx
+++ src/posts/components/post/post.component.tsx
@@ -1,9 +1,19 @@
 import { ReactElement, useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
+import * as dayjs from 'dayjs';
+import LocalizedFormat from 'dayjs/plugin/localizedFormat';
 
+// Components
+import Icon from '../../../shared/ui/icon/icon.component';
+
+// Hooks
 import useDimensions from '../../../shared/hooks/use-dimensions.hook';
 
+// Models
+import { Contributor } from '../../../shared/models/contributor.interface';
 import { Post as PostType } from '../../models/post.interface';
+
+// Utils
 import { setPostSourceGallery } from '../../utils/posts.utils';
 
 import './post.scss';
@@ -16,6 +26,8 @@ const Post = (props: { post: PostType }): ReactElement => {
   const postContainerElem = useRef<HTMLDivElement>(null);
 
   // Component state
+  const [contributor, setContributor] = useState<Contributor | null>(null);
+  const [date, setDate] = useState<string | null>(null);
   const [imgWidth, setImgWidth] = useState<number>(0);
   const [imgSrc, setImgSrc] = useState<string | undefined>(undefined);
   const [loaded, setLoaded] = useState<boolean>(false);
@@ -25,6 +37,8 @@ const Post = (props: { post: PostType }): ReactElement => {
   useEffect(() => {
     // Using window.requestAnimationFrame allows an action to be take after the next DOM paint
     window.requestAnimationFrame(() => setMounted(true));
+
+    dayjs.extend(LocalizedFormat);
   }, []);
 
   // Effect on dimensions change.
@@ -47,41 +61,108 @@ const Post = (props: { post: PostType }): ReactElement => {
     }
   }, [imgSrc, imgWidth, loaded, mounted]);
 
+  useEffect(() => {
+    if (props.post) {
+      // Set post date
+      // console.log('timestamp', props.post.timestamp);
+      setDate(dayjs.unix(props.post.timestamp).format('LL'));
+
+      // Get post contributor
+      if (process.env.REACT_APP_CONTRIBUTORS && props?.post?.tags) {
+        const contributors: Contributor[] = JSON.parse(
+          process.env.REACT_APP_CONTRIBUTORS
+        );
+        // Iterate through contributor array
+        for (const contributor of contributors) {
+          const matchedTag = props.post.tags.find(
+            (tag: string) => tag === contributor.tag
+          );
+          if (matchedTag) {
+            // Set contributor on matched tumblr post tag
+            setContributor(contributor);
+            break;
+          }
+        }
+      }
+    }
+  }, [props.post]);
+
   return (
     <article ref={postElem} className="post">
       <div ref={postContainerElem} className="post-container">
-        <div className="post-container-caption">
-          <div className="post-container-caption-content">
-            {props?.post?.summary && (
-              <Link
-                to={'/post/' + props.post.id_string}
-                className="post-container-caption-content-title"
-              >
-                {props?.post?.summary}
-              </Link>
-            )}
-            {props?.post?.tags?.map((tag: string) => (
-              <Link
-                key={tag}
-                to={'/tagged/' + tag}
-                className="post-container-caption-content-tag"
-              >
-                #{tag}
-              </Link>
-            ))}
-          </div>
-        </div>
-        <Link
-          to={'/post/' + props.post.id_string}
-          className="post-container-photo"
-        >
+        <div className="post-container-photo">
           <img
             alt={props?.post?.caption}
             src={imgSrc}
             onLoad={() => setLoaded(true)}
             className="post-container-photo-src"
           />
-        </Link>
+          <div className="post-container-photo-overlay">
+            <Link
+              to={'/post/' + props.post.id_string}
+              className="post-container-photo-overlay-link"
+            />
+            <div className="post-container-photo-overlay-group">
+              <Link
+                to={'/post/' + props.post.id_string}
+                className="post-container-photo-overlay-group-notes"
+              >
+                <Icon
+                  classes="fas fa-heart"
+                  size={16}
+                  style={{ color: 'rgb(226, 72, 85)' }}
+                />
+                <span className="post-container-photo-overlay-group-text">
+                  {props?.post?.note_count}
+                </span>
+              </Link>
+              {contributor && (
+                <a
+                  href={contributor.href}
+                  className="post-container-photo-overlay-group-contributor"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  <Icon
+                    classes="fas fa-camera"
+                    size={16}
+                    style={{ color: 'white' }}
+                  />
+                  <span className="post-container-photo-overlay-group-text">
+                    {contributor.name}
+                  </span>
+                </a>
+              )}
+            </div>
+            <div className="post-container-photo-overlay-content">
+              {date && (
+                <Link
+                  to={'/post/' + props.post.id_string}
+                  className="post-container-photo-overlay-content-date"
+                >
+                  {date}
+                </Link>
+              )}
+              <Link
+                to={'/post/' + props.post.id_string}
+                className="post-container-photo-overlay-content-title"
+              >
+                {props?.post?.summary}
+              </Link>
+              <div className="post-container-photo-overlay-content-tags">
+                {props?.post?.tags?.map((tag: string) => (
+                  <Link
+                    key={tag}
+                    to={'/tagged/' + tag}
+                    className="post-container-photo-overlay-content-tags-item"
+                  >
+                    #{tag}
+                  </Link>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </article>
   );
